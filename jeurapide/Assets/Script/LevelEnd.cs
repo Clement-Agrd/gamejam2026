368c1583a44a8352d1c430cb1e11347abb5138ca
clement <clement.augoyard@gmail.com>
2026-02-03 11:39:11 +0100

    menu

--- jeurapide/Assets/Script/LevelEnd.cs
+++ jeurapide/Assets/Script/LevelEnd.cs
@@ -3,9 +3,29 @@
 public class LevelEnd : MonoBehaviour
 {
     public int niveauActuel = 1;
+    public GameObject PanelFinDuNiveau;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        PanelFinDuNiveau.SetActive(false);
+        Time.timeScale = 1f;
+        Cursor.visible = false;
+    }
+
+    public void FermerFinDuNiveau()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        PanelFinDuNiveau.SetActive(false);
+        Time.timeScale = 1f;
+        Cursor.visible = false;
+    }
 
     public void FinDuNiveau()
     {
+        Cursor.lockState = CursorLockMode.None;
         LevelProgression.DebloquerNiveau(niveauActuel + 1);
+        PanelFinDuNiveau.SetActive(true);
+        Time.timeScale = 0f;
+        Cursor.visible = true;
     }
 }
